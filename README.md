214e3b2262e51b1a029bc47988c01d066cc4e15d
zweack <jeet88833@gmail.com>
2020-09-12 11:57:34 +0530

    initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+# HackAcademy Demo: Spark Lineage
+
+After installing all dependencies, run the app by entering its folder and typing:
+
+`$ python routes.py`
---
+++ routes.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# two decorators, same function
+@app.route('/')
+@app.route('/index.html')
+def index():
+    filename = os.path.join(app.static_folder, 'data-example.json')
+    return render_template('index.html', the_title='Spark Lineage', filename = filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
